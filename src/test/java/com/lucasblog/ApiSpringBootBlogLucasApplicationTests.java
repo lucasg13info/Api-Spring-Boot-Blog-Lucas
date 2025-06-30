289603cbe3aaad530bdf4cc1e687cb826ae13472
Lucas Estefano <lucccasestefano1@gmail.com>
2025-06-30 12:35:16 +1000

    Started Spring Boot API

---
+++ src/test/java/com/lucasblog/ApiSpringBootBlogLucasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucasblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSpringBootBlogLucasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
